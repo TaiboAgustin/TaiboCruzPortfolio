3f319853e7fd1f2b8163e8d108cddb4578e95fec
Taibo Agustin <taibocruz@gmail.com>
2021-11-26 21:40:33 -0300

    proyecto finalizado

---
+++ main.js
@@ -0,0 +1,19 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector ('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+menuBtn.addEventListener('click', () =>{
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+closeBtn.addEventListener('click',() =>{
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+})
